e15b083824bec44bc34f7e1844761e61904d1580
chenming8 <chenming8@xiaomi.com>
2020-11-25 19:00:20 +0800

    update: on Dell

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Welcome To This Shop App</h1>
+        </main>
+      </Container>
+      <Footer />
+    </>
   );
 }
 
